d9629c1a5677e6f2eebe2151d8b9d89cdc3accf7
igorc22 <igorcamaracc@gmail.com>
2024-01-31 00:12:47 -0300

    Interface web parcialmente pronta

--- app.py
+++ app.py
@@ -7,17 +7,43 @@ app=Flask(__name__)
 
 ## Load the model
 sentiment_model= pickle.load(open('Logistic_Regression_model.pkl','rb'))
-preprocess_function = pickle.load(open('preprocess_text.pkl', 'rb'))
+
+# Preprocess Function
+
+def preprocess_text(text):
+    
+    # Removendo caracteres especiais
+    text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
+    
+    # Removendo rastros de html (como gifs por exemplo)
+    pattern = re.compile(r'<[^>]+>')
+    text = re.sub(pattern, '', text)
+    
+    # Remover tags de spoiler entre colchetes
+    text = re.sub('\[[^]]*\]', '', text)
+    
+    # Tokenizar o texto
+    tokens = word_tokenize(text)
+    
+    # Remover stopwords e converter para minúsculas
+    stopwords_list = set(stopwords.words('english'))
+    tokens = [token.lower() for token in tokens if token.lower() not in stopwords_list]
+    
+    # Aplicar lematização
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    
+    return tokens
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',method=['POST'])
+@app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
     print(data)
-    new_data = preprocess_function(data)
+    new_data = preprocess_text(data)
     output= sentiment_model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
